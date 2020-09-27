5f1ee36760ecf1d90ff5310cfec5ceebd5a6b30b
thomas192 <65908739+thomas192@users.noreply.github.com>
2020-09-27 10:34:29 +0200

    Ajout fonction resumerJour() au DAO

--- NeoService/src/accesseur/NeoDAO.php
+++ NeoService/src/accesseur/NeoDAO.php
@@ -40,4 +40,20 @@
       return $requeteResumerMois->fetchAll(PDO::FETCH_OBJ);
     }
 
+    function resumerJour(int $annee, int $mois, int $jour) {
+      require_once "basededonnees.php";
+
+      $RESUMER_MOIS = "SELECT min(distance) as minimum, avg(distance) as moyenne,
+      max(distance) as maximum FROM neo WHERE date_part('day', date) = :jour
+      AND date_part('month', date) = :mois AND date_part('year', date) = :annee
+      GROUP BY date_part('month', date)";
+      $requeteResumerMois = $basededonnees->prepare($RESUMER_MOIS);
+      $requeteResumerMois->bindParam(':annee', $annee, PDO::PARAM_INT);
+      $requeteResumerMois->bindParam(':mois', $mois, PDO::PARAM_INT);
+      $requeteResumerMois->bindParam(':jour', $jour, PDO::PARAM_INT);
+      $requeteResumerMois->execute();
+
+      return $requeteResumerMois->fetchAll(PDO::FETCH_OBJ);
+    }
+
 }
--- NeoService/src/annee/mois/jour/index.php
+++ NeoService/src/annee/mois/jour/index.php
@@ -1,1 +1,6 @@
 <?php
+  require_once "../../../accesseur/NeoDAO.php";
+
+  $neoDAO = new NeoDAO();
+  $resumeJour = $neoDAO->resumerJour(2020, 1, 1);
+  print_r($resumeJour);
